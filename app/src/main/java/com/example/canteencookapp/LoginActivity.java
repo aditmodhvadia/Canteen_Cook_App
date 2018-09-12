7f51b2877cf3db1d7c1c56bd0325e2e041180f9c
Adit Modhvadia <adit.modhvadia@gmail.com>
2018-09-12 18:25:54 +0530

    add login layout

---
+++ app/src/main/java/com/example/canteencookapp/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.canteencookapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText idEditText;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        idEditText = findViewById(R.id.idEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+
+    }
+}
