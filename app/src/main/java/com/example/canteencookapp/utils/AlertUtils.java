26da42ed6e2ad93d7c123d3a3c739fa7d1b1b60c
Adit Modhvadia <adit.modhvadia@gmail.com>
2019-06-17 02:20:36 +0530

    - add base package for activity and fragment with Mvp - add utils package with AlertDialogs

---
+++ app/src/main/java/com/example/canteencookapp/utils/AlertUtils.java
@@ -0,0 +1,82 @@
+package com.example.canteencookapp.utils;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.canteencookapp.R;
+import com.fazemeright.canteen_app_models.models.FoodItem;
+
+
+/**
+ * To show dialog, toast, snack-bar or any other alert
+ */
+public class AlertUtils {
+
+
+    /**
+     * To get simple alert box
+     *
+     * @param context    App context
+     * @param title      Title
+     * @param desc       Desc
+     * @param buttonText Button text
+     * @param listener   callback
+     * @return @{@link DialogSimple}
+     */
+    public static DialogSimple getAlertBox(Context context, String title, String desc, String buttonText, DialogSimple.AlertDialogListener listener) {
+        DialogSimple simpleDialog = new DialogSimple(context, title, desc, buttonText);
+        simpleDialog.setAlertDialogListener(listener);
+        simpleDialog.setCancelable(false);
+        return simpleDialog;
+    }
+
+    /**
+     * To show @{@link DialogSimple}
+     *
+     * @param context    App context
+     * @param title      Title
+     * @param desc       Desc
+     * @param buttonText Button text
+     * @param listener   callback
+     */
+    public static void showAlertBox(Context context, String title, String desc, String buttonText, DialogSimple.AlertDialogListener listener) {
+        DialogSimple simpleDialog = getAlertBox(context, title, desc, buttonText, listener);
+        simpleDialog.show();
+    }
+
+    public static DialogConfirmation getConfirmationDialog(Context context, String title, String desc, String positiveBtnText, String negativeBtnText, DialogConfirmation.ConfirmationDialogListener listener) {
+        DialogConfirmation dialogConfirmation = new DialogConfirmation(context, title, desc);
+        dialogConfirmation.setPositiveButtonText(positiveBtnText);
+        dialogConfirmation.setNegativeButtonText(negativeBtnText);
+        dialogConfirmation.setConfirmationDialogListener(listener);
+        dialogConfirmation.setCancelable(false);
+        return dialogConfirmation;
+    }
+
+    public static void showConfirmationDialog(Context context, String title, String desc, String positiveBtnText, String negativeBtnText,
+                                              DialogConfirmation.ConfirmationDialogListener listener) {
+        DialogConfirmation dialogConfirmation = getConfirmationDialog(context, title, desc, positiveBtnText, negativeBtnText, listener);
+        dialogConfirmation.show();
+    }
+
+    public static void displaySnackBar(View view, String message) {
+        Snackbar.make(view, message, Snackbar.LENGTH_LONG).show();
+    }
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+    public static Snackbar getSnackBar(View view, String message, String action, View.OnClickListener clickListener) {
+        return Snackbar.make(view, message, Snackbar.LENGTH_LONG).setAction(action, clickListener);
+
+    }
+
+    public static void displaySnackBarWithTime(View view, String message, String action, int timeMilliSecond, View.OnClickListener clickListener) {
+        Snackbar.make(view, message, timeMilliSecond).setAction(action, clickListener).show();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/canteencookapp/utils/OnDialogButtonClickListener.java
@@ -0,0 +1,9 @@
+package com.example.canteencookapp.utils;
+
+public interface OnDialogButtonClickListener {
+
+    void onPositiveButtonClicked();
+
+    void onNegativeButtonClicked();
+}
+
